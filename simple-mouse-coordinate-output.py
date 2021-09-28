cd6ed7aceb9cdac35d631b3134193bf686afe7fb
Beware4 <84943184+Beware4@users.noreply.github.com>
2021-09-27 21:57:31 -0400

    Create simple-mouse-coordinate-output.py

---
+++ simple-mouse-coordinate-output.py
@@ -0,0 +1,6 @@
+import pyautogui #you need to pip install pyautogui for this.
+
+i = 1
+
+while i < 5: #Any number except for 1.
+  print(pyautogui.position()) #This is honestly super easy. You could just program it yourself but why not copy my code instead?
